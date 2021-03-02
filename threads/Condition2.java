74bc55115a9b5c6315f35aeff0ce80a0d2a30492
Stephen Ghiandoni <stephenmghiandoni@gmail.com>
2021-03-02 01:33:10 -0500

    Added test cases 1,2 and 4

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -33,17 +33,17 @@ public class Condition2 {
 	 * automatically reacquire the lock before <tt>sleep()</tt> returns.
 	 */
 	public void sleep() {
-		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
-		boolean intStatus = Machine.interrupt().disable();
-
-		KThread t = KThread.currentThread();
-		waitQueue.add(t);
-		conditionLock.release();
-		KThread.sleep();
-		conditionLock.acquire();
-		Machine.interrupt().restore(intStatus);
-	}
+		//Lib.assertTrue(conditionLock.isHeldByCurrentThread())
+		if(conditionLock.isHeldByCurrentThread()){
+			boolean intStatus = Machine.interrupt().disable();
+			conditionLock.release();
+			KThread t = KThread.currentThread();
+			waitQueue.add(t);
+			KThread.sleep();
+			conditionLock.acquire();
+			Machine.interrupt().restore(intStatus);
+		}
+		}
 
 	/**
 	 * Wake up at most one thread sleeping on this condition variable. The
@@ -74,39 +74,104 @@ public class Condition2 {
 		Lock lock = new Lock();
 		Condition2 cTest = new Condition2(lock);
 
+
+		//***************TEST CASE 1**********************
 		KThread test1 = new KThread(new Runnable(){
 			public void run(){
-				//program crashes here, error is expected but program should continue
-				System.out.println("Test Case 1: The lock is not present on the thread.");
+				System.out.println("\n\nTest Case 1: The lock is not present on the thread.");
+				lock.acquire();
 				lock.release();
+				System.out.println("Attempting to sleep without lock...");
 				cTest.sleep();
+				//verify cTest is not sleeping
+				KThread t = KThread.currentThread();
+				if(t.getStatus() != 3)
+					System.out.println("Test 1 Successful! This thread did not fall asleep.\n");
 			}
 		});
-//
-//		test1.setName("Condition2 test 1");
-//		test1.fork();
-//		test1.join();
-//
 
-		KThread test2 = new KThread(new Runnable(){
+		test1.setName("Condition2 test 1");
+		test1.fork();
+		test1.join();
+
+
+		//***************TEST CASE 2**********************
+		//Put the thread to sleep on one thread. Check the queue size and wake it on another.
+		KThread test2a = new KThread(new Runnable(){
 			public void run(){
 				System.out.println("Test Case 2: The thread is added to the queue.");
 				System.out.println("Current queue size: " + cTest.getQueueSize() + "\nAcquiring lock and putting thread to sleep...");
 				lock.acquire();
 				cTest.sleep();
-				System.out.println("Thread asleep. Queue size: " + cTest.getQueueSize() + "\n");
 				lock.release();
 			}
 		});
-		test2.setName("Condition2 test 2");
-		test2.fork();
-		test2.join();
+		test2a.setName("Condition2 test 2");
+		test2a.fork();
+
+		KThread test2b = new KThread(new Runnable(){
+			public void run(){
+				System.out.println("Thread asleep. Queue size: " + cTest.getQueueSize());
+				lock.acquire();
+				if(cTest.getQueueSize() > 0)
+					System.out.println("Test 2 Successful! Sleeping Thread was added to the queue.\n");
+
+				cTest.wakeAll();
+				lock.release();
+			}
+		});
+
+
+		test2b.fork();
+		test2b.join();
+		test2a.join();
+
+
+		//***************TEST CASE 3**********************
+		KThread test3 = new KThread(new Runnable(){
+			public void run(){
+				
+			}
+		});
+
+
+		//***************TEST CASE 4**********************
+		KThread test4a = new KThread(new Runnable(){
+			public void run(){
+				System.out.println("Test Case 4: The thread is added to the queue.");
+				lock.acquire();
+				cTest.sleep();
+				lock.release();
+			}
+		});
+
+		test4a.setName("Test4");
+		test4a.fork();
+
+		KThread test4b = new KThread(new Runnable(){
+			public void run(){
+				lock.acquire();
+				if(test4a.getStatus() == 3)
+					System.out.println("Status of thread " + test4a.toString() + " is blocked.");	
+				if(cTest.getQueueSize() > 0){
+					System.out.println("Waking next thread on queue.");
+					cTest.wakeAll();
+				}
+				if(test4a.getStatus() == 1)//status ready
+					System.out.println("Test 4 Successful! Thread " + test4a.toString() + " is awake.\n");
+				lock.release();
+			}
+		});
+
+		test4b.fork();
+		test4b.join();
+		test4a.join();
 	}
 
-	public int getQueueSize(){
+	private int getQueueSize(){
 		return waitQueue.size();
 	}
 
 	private Lock conditionLock;
 	private LinkedList<KThread> waitQueue = null;
-}
+	}
